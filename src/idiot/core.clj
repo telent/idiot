66070e5f9bbff79105138187133f27ac7b00a8f7
Daniel Barlow <dan@telent.net>
2019-04-12 15:45:57 +0100

    rudimentary message database

--- src/idiot/core.clj
+++ src/idiot/core.clj
@@ -1,6 +1,25 @@
 (ns idiot.core
   (:gen-class))
 
+;;; make a (hacky, low-budget) database in which we can store
+;;; incoming messages
+
+(def message-store-key
+  (juxt :topic :ts #(java.lang.System/identityHashCode %)))
+
+(defn ->comparator [keyfn]
+  (fn [x y] (compare (keyfn x) (keyfn y))))
+
+(def the-message-store
+  (atom (sorted-set-by (->comparator message-store-key))))
+
+(defn store-message [m]
+  (swap! the-message-store conj m))
+
+;;; search the store for messages on a given topic
+(defn messages-for-topic [topic]
+  (subseq @messages  >= {:topic topic} < {:topic (str topic "~")}))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
